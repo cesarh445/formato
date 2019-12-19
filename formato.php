1dd87fcf091805d9c1d1e4e4bc781ea2e63d3fa7
cesarh445 <52983425+cesarh445@users.noreply.github.com>
2019-12-18 22:51:36 -0600

    a

---
+++ formato.php
@@ -0,0 +1,82 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+		
+		<title>Formato</title>
+	</head>
+	<body>
+		<div class="container">          
+			<form>
+				<H1>CORDINADORA DE TRANSPORTE  GRUPO AZZ</H1>
+				<label>folio <input></label>
+				<label>Orden de trabajo temporal</label>
+				<label>POR:<input> Días</label>
+				<label>UNIDAD:<input></label>
+				<label>NOMBRE DEL OPERADOR:<input></label>
+			</form>
+			<div>
+			<form>
+			<label>Valido del:<input type="date"></label>
+			<label>Hasta el:<input type="date"></label>
+			</form>
+			</div>
+		</div>
+		<div class="container">
+		<form>
+		<label>TIPO DE LICENCIA:(SERVICIO PUBLICO),(FEDERAL)</label>
+		<label>FECHA DE VENCIMIENTO<input type="date"></label>
+		</form>
+		</div>
+		<div class="container">
+			<p>Reporte de motivos de
+				<form>
+				    <label class="checkbox-inline">
+						<input type="checkbox" value="">PERDIDA DE VIAJE 
+					</label>
+					<label class="checkbox-inline">
+						<input type="checkbox" value="">FALTA DE UNIFORME 
+					</label>
+					<label class="checkbox-inline">
+						<input type="checkbox" value="">FALLAS MECANICAS 
+					</label>
+					<label class="checkbox-inline">
+						<input type="checkbox" value="">FUERA DE RUTA
+					</label>
+					<BR>
+					<label>PIRATAJE<input type="checkbox"></label>
+					<label>EXCESO DE VELOCIDAD<input type="checkbox"></label>
+					<label>DEJO PASAJE<input type="checkbox"></label>
+					<label>CHOQUE<input type="checkbox"></label>
+					<BR>
+					<label>ACCIDENTE <input type="checkbox"></label>
+					<label>ABANDONO DE UNIDAD<input type="checkbox"></label>
+					<label>LLEVA ACOMPAÑANTE<input type="checkbox"></label>
+					<label>SIN TARJETA  AL DIA<input type="checkbox"></label>
+				</form>
+			</div>
+			<div class="container">
+				<label for="comment">DESCRIPCION DE LOS HECHOS:</label>
+				<textarea class="form-control" rows="3" id="comment"></textarea>
+			</div>
+			
+			<div class="container">
+				<p>Firma de quien hizo el reporte
+				<p> jefe de personal
+				<p>Firma del operador
+				
+				</div>
+				<form>
+				</form>
+			<!-- Optional JavaScript -->
+			<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+			<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+			<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+			<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+		</body>
+	</html>						
