4d0f394c8dcaf62fafc845bb13bb4f1eaecf85df
cesarh445 <52983425+cesarh445@users.noreply.github.com>
2020-01-15 23:47:13 -0600

    formato elevadores

---
+++ ELEVADORES SCHINDLER/Reporte.php
@@ -0,0 +1,121 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+		<link rel="stylesheet" type="text/css" href="css/estilos.css">
+		<title>Reporte</title>
+		
+	</head>
+	<body>
+		
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-10">
+					<center>
+						<h4>ELEVADORES SCHINDLER
+							<p>SEGURIDAD, HIGIENE Y  MEDIO AMBIENTE
+								<p>REPORTE Y ELIMINACION DE CONDICIONES INSEGURAS
+								</h4>
+							</center>
+						</div>
+						<div class="col-sm-2">
+							
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="container">
+			<form class="form-group">
+			    <div class="form-group">
+					<label id="date">Fecha: <label>
+						<input type="date" class="form-control">
+					</div>
+					<br>
+					<div class="input-group mb-3">
+						<div class="input-group-prepend">
+							<span class="input-group-text">Encontrada por</span>
+						</div>
+						<input type="text" class="form-control">
+					</div>
+					<div class="form-row">
+						<div class="form-group col-md-6">
+							<div class="input-group mb-9">
+								<div class="input-group-prepend">
+									<span class="input-group-text">Area</span>
+								</div>
+								<input type="text" class="form-control">
+							</div>
+						</div>
+						<div class="form-group col-md-6">
+							<div class="input-group mb-3">
+								<div class="input-group-prepend">
+									<span class="input-group-text">No. Equipo</span>
+								</div>
+								<input type="text" class="form-control">
+							</div>
+						</div>
+					</div>
+					<div class="form-group">
+						<div class="input-group mb-3">
+							<div class="input-group-prepend">
+								<span class="input-group-text">Sucursal</span>
+							</div>
+							<input type="text" class="form-control">
+						</div>
+					</div>
+					<div class="form-group">
+						<div class="input-group mb-3">
+							<div class="input-group-prepend">
+								<span class="input-group-text">Dirección</span>
+							</div>
+							<input type="text" class="form-control">
+						</div>
+					</div>
+					<div class="form-group">
+						<div class="input-group mb-3">
+							<div class="input-group-prepend">
+								<span class="input-group-text">Instalación</span>
+							</div>
+							<input type="text" class="form-control">
+						</div>
+					</div>
+					<div class="form-group">
+						<div class="input-group mb-3">
+							<div class="input-group-prepend">
+								<span class="input-group-text">Resuelto por</span>
+							</div>
+							<input type="text" class="form-control">
+						</div>
+					</div>
+					<br>
+					<div class="form-check-inline">
+						<label class="form-check-label">
+							<input type="checkbox" class="form-check-input" value="">Técnico
+						</label>
+					</div>
+					<div class="form-check-inline">
+						<label class="form-check-label">
+							<input type="checkbox" class="form-check-input" value="">Supervisor
+						</label>
+					</div>
+					<div class="form-check-inline">
+						<label class="form-check-label">
+							<input type="checkbox" class="form-check-input">Seguridad
+						</label>
+					</div>
+					
+					</form>
+					
+				</div>
+				
+			</body>
+			<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+			<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+			<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+		</html>																																															
